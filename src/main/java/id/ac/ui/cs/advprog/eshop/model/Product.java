2e24c112420881659b4a10e38dc2bd482b188016
adlynfakhreyz <adlynuniacc@gmail.com>
2025-02-14 12:38:32 +0700

    fix typo

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter @Setter
 public class Product {
-    private String prodcutId;
+    private String productId;
     private String productName;
     private int productQuantity;
 }
