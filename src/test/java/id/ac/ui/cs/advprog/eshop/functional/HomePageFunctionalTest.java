ace07c8e7c3926e3d6607951a87bd47f32f6c3d7
adlynfakhreyz <adlynuniacc@gmail.com>
2025-02-21 13:33:07 +0700

    fix: remove unsued throw exception declaration

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
@@ -31,7 +31,7 @@ class HomePageFunctionalTest {
     }
 
     @Test
-    void pageTitle_isCorrect(ChromeDriver driver) throws Exception {
+    void pageTitle_isCorrect(ChromeDriver driver) {
         // Exercise
         driver.get(baseUrl);
         String pageTitle = driver.getTitle();
@@ -41,7 +41,7 @@ class HomePageFunctionalTest {
     }
 
     @Test
-    void welcomeMessage_homePage_isCorrect(ChromeDriver driver) throws Exception {
+    void welcomeMessage_homePage_isCorrect(ChromeDriver driver) {
         // Exercise
         driver.get(baseUrl);
         String welcomeMessage = driver.findElement(By.tagName("h3"))
