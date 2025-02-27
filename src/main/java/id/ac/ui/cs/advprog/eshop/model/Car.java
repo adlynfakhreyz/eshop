a2d02777f3b35c0b291160519646a8a1983157bf
adlynfakhreyz <adlynuniacc@gmail.com>
2025-02-28 03:31:48 +0800

    feat: Adds Car modeel

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop.model;
+import lombok.getter;
+import lombok.setter;
+
+@Getter @Setter
+public class Car {
+    private String id;
+    private String carName;
+    private String carColor;
+    private int carQuantity;
+}
