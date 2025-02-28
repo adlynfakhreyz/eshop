83a0f5d39f2a3ac9ebe1d40775b25d37eb9d106d
adlynfakhreyz <adlynuniacc@gmail.com>
2025-02-28 10:03:20 +0800

    fix: update controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -1,13 +1,16 @@
 package id.ac.ui.cs.advprog.eshop.controller;
-
+import id.ac.ui.cs.advprog.eshop.model.Car;
 import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
+
 
 import java.util.List;
 
@@ -70,8 +73,12 @@ public class ProductController {
 @RequestMapping("/car")
 class CarController extends ProductController {
 
-    @Autowired
-    private CarServiceImpl carservice;
+    private final CarServiceImpl carservice;
+
+    public CarController(ProductService service, CarServiceImpl carservice) {
+        super(service);
+        this.carservice = carservice;
+    }
 
     @GetMapping("/createCar")
     public String createCarPage(Model model) {
