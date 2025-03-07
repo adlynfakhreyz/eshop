634b66e3a116573705f427eb39222cd9e72efb93
adlynfakhreyz <adlynuniacc@gmail.com>
2025-03-07 13:32:23 +0700

    [RED] fix Payment Skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,19 +1,16 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Map;
 
-@Builder
 @Getter
 public class Payment {
     private String id;
-    private String method;
-    private String status;
-    private Map<String, String> paymentData;
     private Order order;
+    private String method;
+    private Map<String, String> paymentData;
+    private String status;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData) {
         // Implementation will be added later
